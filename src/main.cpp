0b0b2f1508f2e802c89cb83b829ec2da4ebe69c2
Ryan Frazier <{ID}+{username}@users.noreply.github.com>
2024-11-22 15:08:31 -0700

    Read from 4 pot pins to set speeds and accelerations

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,10 @@
  */
 #include <Arduino.h>
 
-#define POT_PIN A0
+#define FAST_POT_PIN A0
+#define SLOW_POT_PIN A1
+#define ACC_POT_PIN A2
+#define DEC_POT_PIN A3
 #define LED_GREEN_PIN 8
 #define LED_YELLOW_PIN 9
 
@@ -31,15 +34,35 @@
  * 30 -> 227RPM
 */
 
-#define FAST_SPEED 46 
-#define SLOW_SPEED 600
 #define STOP_SPEED 2400
+#define FAST_SPEED_MAX 46
+#define FAST_SPEED_MIN 75
+#define SLOW_SPEED_MAX 600
+#define SLOW_SPEED_MIN 1000
+
+#define DEFAULT_FAST_SPEED 40
+#define DEFAULT_SLOW_SPEED 900
 
 // 20 -> 2 s from FAST to STOP
 // 40 -> 3 s
 // 60 -> 4 s
 // 100 -> 6 s
-#define ACC_FACTOR 40
+#define MAX_FACTOR 2 
+#define MIN_FACTOR 100
+
+#define DEFAULT_ACC_FACTOR 40
+#define DEFAULT_DEC_FACTOR 100 
+
+#define FAST_INDEX 0
+#define SLOW_INDEX 1
+#define ACC_INDEX 2
+#define DEC_INDEX 3
+
+int potPins[] = {FAST_POT_PIN, SLOW_POT_PIN, ACC_POT_PIN, DEC_POT_PIN};
+int potVals[] = {0, 0, 0, 0};
+int previousPotVals[] = {0, 0, 0, 0};
+int settings[] = {DEFAULT_FAST_SPEED, DEFAULT_SLOW_SPEED, DEFAULT_ACC_FACTOR, DEFAULT_DEC_FACTOR};
+const int SETTINGS_RANGES[4][2] = {{FAST_SPEED_MAX, FAST_SPEED_MIN}, {SLOW_SPEED_MAX, SLOW_SPEED_MIN}, {MAX_FACTOR, MIN_FACTOR}, {MAX_FACTOR, MIN_FACTOR}};
 
 enum State {
   STOP,
@@ -53,6 +76,7 @@ int previousPotVal = 0;
 unsigned long t = 0;
 unsigned long last = 0;
 unsigned long delta = 0;
+unsigned long acc_delta = 0;
 unsigned long input_delta = 0;
 
 State currentState = State::STOP;
@@ -60,12 +84,33 @@ unsigned long stopPoint = STOP_SPEED;
 unsigned long period = STOP_SPEED;
 
 State readState();
+void readPots();
+void readPotsTimed();
 void outputLED(State);
 void stepMotor();
 void updateStopPoint();
 void updatePeriod();
 
 void setup() {
+
+  /**
+   * analogRead is really slow and interrupts the stepper motor
+   * speeding up the ADC (Analog to Digital Converter) settings will reduce the clunking noise
+   * if reading analog pins every cycle then speed up the ADC settings
+   * the downside is that you sacrifice accuracy
+   * see http://gammon.com.au/adc
+   */
+  // ADCSRA &= ~(bit (ADPS0) | bit (ADPS1) | bit (ADPS2)); // clear prescaler bits
+
+  // uncomment as required
+  //  ADCSRA |= bit (ADPS0);                               //   2  
+  //  ADCSRA |= bit (ADPS1);                               //   4  
+  //  ADCSRA |= bit (ADPS0) | bit (ADPS1);                 //   8  
+  //  ADCSRA |= bit (ADPS2);                               //  16 
+  //  ADCSRA |= bit (ADPS0) | bit (ADPS2);                 //  32 
+  //  ADCSRA |= bit (ADPS1) | bit (ADPS2);                 //  64 
+  //  ADCSRA |= bit (ADPS0) | bit (ADPS1) | bit (ADPS2);   // 128
+
   pinMode(STEP_PIN, OUTPUT);
   pinMode(LED_GREEN_PIN, OUTPUT);
   pinMode(LED_YELLOW_PIN, OUTPUT);
@@ -75,20 +120,28 @@ void setup() {
   pinMode(SLOW_INPUT_PIN, INPUT_PULLUP);
   pinMode(FAST_INPUT_PIN, INPUT_PULLUP);
 
-  digitalWrite(EN_PIN, LOW);
   digitalWrite(DIR_PIN, HIGH);
   digitalWrite(EN_PIN, HIGH);
-  // digitalWrite(LED_GREEN_PIN, HIGH);
-  // digitalWrite(LED_YELLOW_PIN, HIGH);
+
+  /**
+   * Will need to reset the arduino to read settings
+   */
+  readPots();
 }
 void loop() {
   t = micros();
   delta += t - last;
+  acc_delta += t - last;
   input_delta += t - last;
 
   currentState = readState();
   outputLED(currentState);
   
+  /**
+   * analogRead is really slow and interrupts
+   * the stepper motor
+   */
+  // readPotsTimed();
   updateStopPoint();
   updatePeriod();
   stepMotor();
@@ -106,10 +159,10 @@ void stepMotor() {
 
 void updateStopPoint() {
   if (currentState == State::FAST) {
-    stopPoint = FAST_SPEED;
+    stopPoint = settings[FAST_INDEX];
   }
   if (currentState == State::SLOW) {
-    stopPoint = SLOW_SPEED;
+    stopPoint = settings[SLOW_INDEX];
   }
   if (currentState == State::STOP) {
     stopPoint = STOP_SPEED;
@@ -117,10 +170,11 @@ void updateStopPoint() {
 }
 
 void updatePeriod() {
-  if (input_delta > 10000) {
+  if (acc_delta > 10000) {
+
     // DECELERATE
     if (period < stopPoint) {
-      unsigned int val = int(trunc(period/ACC_FACTOR));
+      unsigned int val = int(trunc(period/settings[3]));
       period += (1 + val);
       // This can overshoot the STOP_SPEED and not stop
       if (period > STOP_SPEED - val - 2) {
@@ -129,9 +183,8 @@ void updatePeriod() {
     }
     // ACCELERATE
     if (period > stopPoint) {
-      period -= (1 + int(trunc(period/ACC_FACTOR)));
+      period -= (1 + int(trunc(period/settings[2])));
     }
-
 
     if (period == STOP_SPEED) {
       digitalWrite(EN_PIN, HIGH);
@@ -139,7 +192,7 @@ void updatePeriod() {
       digitalWrite(EN_PIN, LOW);
     }
 
-    input_delta = 0;
+    acc_delta = 0;
   }
 }
 
@@ -151,6 +204,27 @@ State readState() {
     return State::FAST;
   } else {
     return State::STOP;
+  }
+}
+
+void readPots() {
+    potVals[0] = analogRead(FAST_POT_PIN);
+    potVals[1] = analogRead(SLOW_POT_PIN);
+    potVals[2] = analogRead(ACC_POT_PIN);
+    potVals[3] = analogRead(DEC_POT_PIN);
+    for (byte i = 0; i < 4; i++) {
+      if (potVals[i] - previousPotVals[i] > 2 || potVals[i] - previousPotVals[i] < -2) {
+        settings[i] = map(potVals[i], 0, 1023, SETTINGS_RANGES[i][0], SETTINGS_RANGES[i][1]);
+        previousPotVals[i] = potVals[i];
+      }
+    }
+}
+
+void readPotsTimed() {
+  if (input_delta > 1000000) {
+    readPots();
+
+    input_delta = 0;
   }
 }
 
